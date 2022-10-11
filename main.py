af1174a038bbc643bbe10f70abfa2fad82468647
Abhishek-BG <abhigowda0190@gmail.com>
2022-10-11 21:39:12 +0530

    update

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('logged in as {0.user}'.format(client))
  
-@client.event
+@client.eventxss
 async def on_message(message):
     if message.author == client.user:
         return
  
     if message.content.startswith('hi'):
         await message.channel.send('Hello!')
+    
  
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-client.run("MTAyOTQxMjI0ODkyMzQwNjM2Ng.GzDmb2.relNxyQbTeFMVm_vaCzcqYjBarsRu0QyX4OxLE")
+# client.run("MTAyOTQxMjI0ODkyMzQwNjM2Ng.GoCrIo.ZbUTnn_ebiUkBT-JONBfAoDYaMJ23lW-xOWPgc")
